53c4da4f5f53e6d756c131f5e4f4e27575703374
fchimbera <chimberafreeman@gmail.com>
2025-07-06 15:56:09 +0200

    updated batchprocessing

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -25,12 +25,24 @@ def stream_users_in_batches(batch_size):
     cursor.close()
     connection.close()
 
+count = 0
 
 def batch_processing(batch_size):
     """
     Processes each batch to filter users over the age of 25
     """
+    global count
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
                 print(user)
+                count += 1
+
+if __name__ == "__main__":
+    batch_size = 10  # Define the batch size
+    batch_processing(batch_size)
+    if count == 0:
+        print("No users over the age of 25 found.")
+    else:
+        print(f"Total users over the age of 25: {count}")
+    print("Batch processing completed.")    
