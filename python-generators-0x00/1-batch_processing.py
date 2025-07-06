ba45402c7bc562d6982e11a19e51a3b4f89e441f
fchimbera <chimberafreeman@gmail.com>
2025-07-06 15:26:15 +0200

    Revert "update gitignore"
    This reverts commit 33e6f9601920d86bc13468673cff65b88da7907f.

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -7,10 +7,7 @@ def stream_users_in_batches(batch_size):
     """
     connection = connect_to_prodev()
     if not connection:
-        print("Failed to connect to the database.")
         return
-    else:
-        print("Connected to the database successfully.")
 
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
@@ -28,22 +25,12 @@ def stream_users_in_batches(batch_size):
     cursor.close()
     connection.close()
 
-count = 0
 
 def batch_processing(batch_size):
     """
     Processes each batch to filter users over the age of 25
     """
-    global count
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
                 print(user)
-                count += 1
-
-if __name__ == "__main__":
-    print("--- Starting main execution of 1-batch_processing.py ---")
-    batch_processing(2) # Example: process in batches of 2
-    # You might want to try different batch sizes like 1, 5, or 10
-    print(f"Total users processed: {count}")
-    print("--- Script 1-batch_processing.py finished ---")
