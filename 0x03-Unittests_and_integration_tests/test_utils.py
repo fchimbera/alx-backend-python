b32406d1ea4fa539d64d07c29d2f3797b2a65ec8
fchimbera <chimberafreeman@gmail.com>
2025-07-19 02:33:51 +0200

    updated test_utils with test memorize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -99,7 +99,7 @@ class TestMemoize(unittest.TestCase):
                 """
                 return self.a_method()
 
-        with patch.object(TestClass,'a_method',return_value=42) as mock_method:
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
             test_instance = TestClass()
             # Call a_property twice
             result1 = test_instance.a_property
