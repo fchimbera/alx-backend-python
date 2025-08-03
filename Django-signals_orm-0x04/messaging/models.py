d713d413fc41a5babd6fad9f364571ec00a593fb
fchimbera <chimberafreeman@gmail.com>
2025-08-03 17:10:14 +0200

    updated sgnal for logging

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -3,52 +3,56 @@ from django.contrib.auth.models import User
 
 class Message(models.Model):
     """
-    Represents a direct message sent from one user to another.
+    direct message sent from one user to another.
+    Added a 'edited' field to track if the message has been modified.
     """
-    # The user who sent the message.
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
-    
-    # The user who received the message.
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
-    
-    # The content of the message.
     content = models.TextField()
-    
-    # The timestamp when the message was created. auto_now_add=True
-    # automatically sets the creation date.
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False) # New field to track edits
 
     class Meta:
-        # Order messages by timestamp in descending order (most recent first)
         ordering = ['-timestamp']
 
     def __str__(self):
-        """String representation of the Message model."""
         return f'From {self.sender.username} to {self.receiver.username} at {self.timestamp.strftime("%Y-%m-%d %H:%M:%S")}'
 
 
 class Notification(models.Model):
-    """
-    Represents a notification for a user, triggered by an event like
-    receiving a new message.
-    """
-    # The user who will receive the notification.
+    
+    # notification for a user.
+    
     user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
-    
-    # A link to the message that triggered this notification.
     message = models.ForeignKey(Message, related_name='notifications', on_delete=models.CASCADE)
-    
-    # A flag to check if the user has read the notification.
     is_read = models.BooleanField(default=False)
-    
-    # The timestamp when the notification was created.
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        # Order notifications by creation date, most recent first.
         ordering = ['-created_at']
 
     def __str__(self):
-        """String representation of the Notification model."""
         return f'Notification for {self.user.username} about a message from {self.message.sender.username}'
 
+
+class MessageHistory(models.Model):
+    """
+    A new model to store the history of message edits.
+    Each instance represents a previous version of a message.
+    """
+    # Link to the original message that was edited.
+    message = models.ForeignKey(Message, related_name='history', on_delete=models.CASCADE)
+    
+    # The content of the message before it was edited.
+    old_content = models.TextField()
+    
+    # The timestamp when the edit occurred.
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # Order history by the time of the edit, most recent first.
+        ordering = ['-edited_at']
+        verbose_name_plural = "Message Histories"
+
+    def __str__(self):
+        return f'History for Message {self.message.id} at {self.edited_at.strftime("%Y-%m-%d %H:%M:%S")}'
