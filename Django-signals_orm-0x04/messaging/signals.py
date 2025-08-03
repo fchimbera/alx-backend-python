37171163c644a81744a2c6f6e2d34bcaeeaf5d65
fchimbera <chimberafreeman@gmail.com>
2025-08-03 18:30:34 +0200

    updated models.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,27 +1,52 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from django.utils import timezone
 from django.contrib.auth.models import User
+from .models import Message, Notification, MessageHistory
 
-# The @receiver decorator connects the function to the post_save signal of the Message model.
-# This function will be called every time a Message object is saved.
+# The original signal from Task 1
 @receiver(post_save, sender=Message)
 def create_notification_on_new_message(sender, instance, created, **kwargs):
     """
     Signal handler to create a notification when a new message is saved.
-
-    :param sender: The model class that sent the signal (Message).
-    :param instance: The actual instance of Message that was just saved.
-    :param created: A boolean; True if a new record was created, False if an update.
     """
-    # We only want to create a notification if a new message was created,
-    # not when an existing message is updated.
     if created:
-        # The user to be notified is the receiver of the new message.
-        # The message is the instance that just got created.
         Notification.objects.create(
             user=instance.receiver,
             message=instance
         )
         print(f"DEBUG: Notification created for {instance.receiver.username}")
 
+
+# This is the corrected pre_save signal handler for Task 2.
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    """
+    Signal handler to log message content before it is updated.
+    It checks if the message is being updated and if the content has changed.
+    """
+    # Check if the instance has a primary key, meaning it's an existing object being updated.
+    if instance.pk:
+        try:
+            # Fetch the original object from the database before the save operation.
+            original_message = Message.objects.get(pk=instance.pk)
+        except Message.DoesNotExist:
+            # This should not happen in a normal update flow.
+            return
+        
+        # Compare the content. If it has changed, create a history entry.
+        if original_message.content != instance.content:
+            # Create a new MessageHistory record with the old content.
+            # We assume the sender is the one who is editing their own message.
+            MessageHistory.objects.create(
+                message=instance,
+                old_content=original_message.content,
+                edited_by=instance.sender
+            )
+            
+            # Update the edited_at field on the Message instance itself
+            # The edited_by field is also updated here.
+            instance.edited_at = timezone.now()
+            instance.edited_by = instance.sender
+            
+            print(f"DEBUG: Message {instance.pk} updated. Old content logged to history.")
