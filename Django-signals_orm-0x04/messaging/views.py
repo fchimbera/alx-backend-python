45c9d281faa753883c93f54d4a5766f88927ea29
fchimbera <chimberafreeman@gmail.com>
2025-08-03 20:39:27 +0200

    added view cached

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import logout
 from django.contrib import messages
 from .models import Message
 from django.views.generic import ListView
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 @login_required
 def delete_user(request):
@@ -19,20 +21,20 @@ def delete_user(request):
         return redirect('home')
     return HttpResponse("Are you sure you want to delete your account? This action is irreversible.", status=200)
 
-
+@method_decorator(cache_page(60), name='dispatch')
 class InboxView(ListView):
     """
-    A class-based view to display a user's unread messages.
-    Uses the UnreadMessagesManager to get unread messages and the .only()
-    method for optimization.
+    A class-based view to display a user's messages.
+    This view is cached for 60 seconds using the cache_page decorator.
     """
     model = Message
     template_name = 'messaging/inbox.html'
-    context_object_name = 'unread_messages'
+    context_object_name = 'inbox_messages'
 
     def get_queryset(self):
-        
-        return (Message.unread.unread_for_user(self.request.user).only(),
-                Message.unread_objects.filter(
-                receiver=self.request.user).select_related('sender', 'receiver').order_by('-timestamp')
-                )
+        """
+        Fetches all messages for the current user, using select_related for optimization.
+        """
+        return Message.objects.filter(
+            receiver=self.request.user
+        ).select_related('sender', 'receiver').order_by('-timestamp')
