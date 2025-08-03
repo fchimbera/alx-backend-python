523c54ef8818e5bc84329a05eee7eedcb488b923
fchimbera <chimberafreeman@gmail.com>
2025-08-03 18:28:39 +0200

    updated models

--- Django-signals_orm-0x04/messaging_app/messaging/models.py
+++ Django-signals_orm-0x04/messaging_app/messaging/models.py
@@ -3,26 +3,27 @@ from django.contrib.auth.models import User
 
 class Message(models.Model):
     """
-    direct message sent from one user to another.
-    Added a 'edited' field to track if the message has been modified.
+    Represents a direct message sent from one user to another.
     """
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    edited = models.BooleanField(default=False) # New field to track edits
+    # New fields to track edits.
+    edited_by = models.ForeignKey(User, related_name='edited_messages', on_delete=models.SET_NULL, null=True, blank=True)
+    edited_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         ordering = ['-timestamp']
 
     def __str__(self):
-        return f'From {self.sender.username} to {self.receiver.username} at {self.timestamp.strftime("%Y-%m-%d %H:%M:%S")}'
+        return f'From {self.sender.username} to {self.receiver.username}'
 
 
 class Notification(models.Model):
-    
-    # notification for a user.
-    
+    """
+    Represents a notification for a user.
+    """
     user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
     message = models.ForeignKey(Message, related_name='notifications', on_delete=models.CASCADE)
     is_read = models.BooleanField(default=False)
@@ -37,22 +38,17 @@ class Notification(models.Model):
 
 class MessageHistory(models.Model):
     """
-    A new model to store the history of message edits.
-    Each instance represents a previous version of a message.
+    A model to store the history of message edits.
     """
-    # Link to the original message that was edited.
     message = models.ForeignKey(Message, related_name='history', on_delete=models.CASCADE)
-    
-    # The content of the message before it was edited.
     old_content = models.TextField()
-    
-    # The timestamp when the edit occurred.
+    # The user who performed the edit.
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     edited_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        # Order history by the time of the edit, most recent first.
         ordering = ['-edited_at']
         verbose_name_plural = "Message Histories"
 
     def __str__(self):
-        return f'History for Message {self.message.id} at {self.edited_at.strftime("%Y-%m-%d %H:%M:%S")}'
+        return f'History for Message {self.message.id} by {self.edited_by.username} at {self.edited_at}'
