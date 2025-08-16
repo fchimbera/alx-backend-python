eb0bb56a9d8855b5f495df321693666d21e5195a
fchimbera <chimberafreeman@gmail.com>
2025-08-16 23:12:07 +0200

    add dockerfile

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-i@z^fvjkiht!zcysq9vg#+al4c#2i@17%otu^!=in2833xy7^z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
